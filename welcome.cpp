736fff1679d3fe49c10d7a5e35d0aacb68b68c91
Buddhamsaranam <hubbgeet@gmail.com>
2022-12-01 21:03:50 +0545

    MESSAGE TO YOU

---
+++ welcome.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using std::cout;
+using std::endl;
+int main(){
+    cout<<"Welcome to my public repository"<<endl;
+    cout<<"Only one rule"<<endl;
+    cout<<"You write I check i never write "<<endl;
+    return 0;
+}
